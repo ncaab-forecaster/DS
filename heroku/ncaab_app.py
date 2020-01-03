ec0c74f5f3be467e0a5c5f6e08bef8fa3154e69b
gyhou <47550192+gyhou@users.noreply.github.com>
2020-01-02 18:16:24 -0800

    Add files via upload

---
+++ heroku/ncaab_app.py
@@ -0,0 +1,53 @@
+import json
+import pickle
+import sklearn
+import numpy as np
+from flask_cors import CORS
+from flask import Flask, request
+
+
+app = Flask(__name__)
+CORS(app)
+@app.route("/", methods=['POST'])
+def address():
+    '''Takes json dictionaries and return prediction values'''
+
+    # receive input
+    inputs = request.get_json(force=True)
+
+    # get data from json
+    year = inputs['year']
+    month = inputs['month']
+    day = inputs['day']
+    home_name = inputs['home_name']
+    home_rank = inputs['home_rank']
+    away_name = inputs['away_name']
+    away_rank = inputs['away_rank']
+    # validate input
+    # assert isinstance(category, str)
+    # assert isinstance(review, str)
+
+    # unpickle
+    filename = "pipe.pkl"
+    infile = open(filename, "rb")
+    model = pickle.load(infile)
+    infile.close()
+
+    # predict
+    home_score, away_score = model.predict(
+        [year, month, day, home_name, home_rank, away_name, away_rank])
+
+    # use a dictionary to format output for json
+    out = {'home_score': home_score,
+           'away_score': away_score}
+
+    # give output to sender
+    return app.response_class(
+        response=json.dumps(out),
+        status=200,
+        mimetype='application/json'
+    )
+
+
+if __name__ == '__main__':
+    app.run(port=9000, debug=True)
